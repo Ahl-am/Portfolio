61b8cca0f430b68741356ff489a3f8f7c25366e8
Ahl-am <Ahlam.ibra01@gmail.com>
2023-03-16 08:30:59 +0300

    delete redme.md + add basename to router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Experience, Hero, Navbar } from './components';
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div className="relative z-0 bg-primary">
             <div className='bg-hero-pattern bg-cover bg-no-repeate bg-center'>
               <Navbar />
